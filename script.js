9b6aad95e7799fae29dcd9063d45d7e6511c782e
jhonatan <jhonata0200p@gmail.com>
2026-04-24 20:40:26 -0500

    week 3

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const menu = document.getElementById("contenedor-menu");
+const nombre_header = document.querySelector(".nombre_header");
+const nav_a = document.querySelector(".navegacion");
 const claseActiva = document.querySelector(".head");
 
 menu.addEventListener("click", () => {
+  claseActiva.classList.toggle("active");
+});
+
+nav_a.addEventListener("click", () => {
+  claseActiva.classList.remove("active");
+});
+
+nombre_header.addEventListener("click", () => {
   claseActiva.classList.toggle("active");
 });
